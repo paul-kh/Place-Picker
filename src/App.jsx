054c18f832c28eafbb5811461e50fe81b4ed4de8
Paul Chheang <phalcheat@gmail.com>
2024-06-20 11:13:36 -0500

    Handled ESC key when use pressed it to close the Modal

--- src/App.jsx
+++ src/App.jsx
@@ -90,7 +90,7 @@ function App() {
 
   return (
     <>
-      <Modal open={modalIsOpen}>
+      <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
           onConfirm={handleRemovePlace}
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,7 +1,7 @@
 import { useRef, useEffect } from "react";
 import { createPortal } from "react-dom";
 
-function Modal({ children, open }) {
+function Modal({ children, open, onClose }) {
   const dialog = useRef();
 
   // We need useEffect() here because the first time that
@@ -16,8 +16,18 @@ function Modal({ children, open }) {
     }
   }, [open]);
 
+  /* Handle ESC key pressed on the dialog box
+     ******************************************
+     The <dialog> element can also be closed by pressing the ESC key on the keyboard. 
+     In that case, the dialog will disappear but the state passed to the open prop (i.e., the modalIsOpen state)
+     will not be set to false. Therefore, the modal can't be opened again (because modalIsOpen still is true.
+
+    To fix this issue, we must listen to the modal being closed by adding the built-in onClose prop to 
+    the <dialog>. The event is then "forwarded" to the App component by accepting a custom onClose prop on 
+    the Modal component.
+  */
   return createPortal(
-    <dialog className="modal" ref={dialog}>
+    <dialog className="modal" ref={dialog} onClose={onClose}>
       {children}
     </dialog>,
     document.getElementById("modal")
